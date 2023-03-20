45cae61b44d1a7940e5e1e8f617efecba3e79006
shriPy <93673122+shriPy@users.noreply.github.com>
2023-03-20 00:01:15 -0700

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,7 +1,8 @@
 document.addEventListener('DOMContentLoaded', function () {
-  const myButton = document.getElementById('myButton');
-
-  myButton.addEventListener('click', function () {
-    alert('Hello, World!');
+    const subscribeBtn = document.querySelector('.subscribe-btn');
+  
+    subscribeBtn.addEventListener('click', function () {
+      alert('Thank you for subscribing! We\'ll keep you updated on our upcoming drops.');
+    });
   });
-});
+  
